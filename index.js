5f266994d3b9d7597429f2715f11deb100776c6f
KariKorpinen <karikkorpinen@gmail.com>
2020-02-16 23:05:15 +0200

    402

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+
+/*
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+/*
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
 })
-
+*/
+/*
 const Blog = mongoose.model('Blog', blogSchema)
 require('dotenv').config()
 
@@ -45,3 +58,4 @@ app.post('/api/blogs', (request, response) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+*/
